533cb5ec2292ce6d82b75e559ea1d05c06c0cea8
Washington Soares <washingtonudi@gmail.com>
2021-03-15 07:48:29 -0300

    Edit site URL

--- lib/constants.ts
+++ lib/constants.ts
@@ -18,8 +18,8 @@ export const SITE_URL = 'https://sds3.vercel.app';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
 export const TWITTER_USER_NAME = 'vercel';
 export const BRAND_NAME = 'DEVSUPERIOR';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const SITE_NAME_MULTILINE = ['SEMANA DEVSUPERIOR'];
+export const SITE_NAME = 'SEMANA DEVSUPERIOR';
 export const META_DESCRIPTION =
   'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
 export const SITE_DESCRIPTION =
